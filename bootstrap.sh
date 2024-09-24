8322bb2662bdecebc65c48491334d3065783c248
Cameron Devine <cameron.devine@alleninstitute.org>
2024-09-24 12:35:49 -0700

    the zip archive includes the branch name

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,5 @@ mkdir ~/setup
 cd ~/setup
 wget -O TEM_setup.zip https://github.com/AllenInstitute/TEM_setup/archive/refs/heads/master.zip
 unzip TEM_setup.zip
+mv TEM_setup-master TEM_setup
 TEM_setup/setup.sh
