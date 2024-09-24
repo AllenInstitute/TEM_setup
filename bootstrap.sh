4d45ec6b13183c4975265e742163b2a20d05236c
Cameron Devine <cameron.devine@alleninstitute.org>
2024-09-24 12:52:45 -0700

    setup.sh is not executable, so run it with sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@ cd ~/setup
 wget -O TEM_setup.zip https://github.com/AllenInstitute/TEM_setup/archive/refs/heads/master.zip
 unzip TEM_setup.zip
 mv TEM_setup-master TEM_setup
-TEM_setup/setup.sh
+sh TEM_setup/setup.sh
